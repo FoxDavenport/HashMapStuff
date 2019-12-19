6da5e0175644cb1120f6264531efbcc2c504e84f
FoxDavenport <54683756+FoxDavenport@users.noreply.github.com>
2019-12-19 13:05:03 -0800

    Add files via upload

--- HashData.java
+++ HashData.java
@@ -16,19 +16,43 @@ public class HashData
         int exit=0;
         
         // Create a HashTable to store data about users
-        // String values corresponding to integer keys 
-        Map<String, String> UserInfo= new Hashtable<>(); 
+        // String values corresponding to string of arraylist
+        LinkedHashMap<String, List<String>> UserInfo= new LinkedHashMap<>(); 
+        
+        //creates a list to store values for Fox Davenport
+        List<String> FoxDavenport= new ArrayList<String>();
+        FoxDavenport.add("UltimateLifeForm@gmail.com");
+        FoxDavenport.add("Pineapple Pizza");
+        
+        //create a list to store values for Daniel Yang
+        List<String> DanielYang= new ArrayList<String>();
+        DanielYang.add("danielyang@yahoo.com");
+        DanielYang.add("Tyler1");
+        
+        //create a list to store values for Mithil Pujar
+        List<String> MithilPujar= new ArrayList<String>();
+        MithilPujar.add("HugeMuscleMithil@muscle.com");
+        MithilPujar.add("MUSCLE!");
+        
+        //create a list to store values for Mr. John
+        List<String> MrJohn= new ArrayList<String>();
+        MrJohn.add("OurLordAndSavior@holy.com");
+        MrJohn.add("CodePower");
+        
+        //create a list to store values for Alex Clanton
+        List<String> AlexClanton= new ArrayList<String>();
+        AlexClanton.add("BadCoder@terrible.com");
+        AlexClanton.add("CacaPooPoo");
         
         //creates a scanner object for input from user
         Scanner input= new Scanner(System.in);
         
-        //Input some user info
-        //User named Fox Davenport and their email
-        UserInfo.put("Fox Davenport", "UltimateLifeForm@gmail.com");
-        UserInfo.put("Daniel Yang", "danielyang@yahoo.com");
-        UserInfo.put("Mithil", "HugeMuscleMithil@muscle.com");
-        UserInfo.put("Alex", "BadCoder@terrible.com");
-        UserInfo.put("Mr. John", "OurLordandSavior@holy.com");
+        //Input some user info into the map
+        UserInfo.put("Fox Davenport", FoxDavenport);
+        UserInfo.put("Mithil Pujar", MithilPujar);
+        UserInfo.put("Daniel Yang", DanielYang);
+        UserInfo.put("Mr. John", MrJohn);
+        UserInfo.put("Alex Clanton", AlexClanton);
         
         System.out.println("Hi! Welcome to our mini databse! Please input a user you would like to learn about");
         System.out.println("Press 0 when you want to exit");
@@ -36,11 +60,6 @@ public class HashData
         // HashMap's key set
         Set<String> Username = UserInfo.keySet();
         System.out.println("Users: " + Username);
-        
-        //New Hashmap for email link to password
-        Map<String, String> Password= new Hashtable<>();
-        Password.put("UltimateLifeForm@gmail.com", "Password");
-        Password.put("danielyang@yahoo.com", "Tyler1");
         
         //loops till user wants to exit
         while (exit==0){
@@ -54,19 +73,18 @@ public class HashData
         
         if(user==1) {
             System.out.println("Fox's info: ");
-            System.out.println(UserInfo.get("Fox Davenport"));
-            System.out.println("Their password is: " + Password.get("UltimateLifeForm@gmail.com") + "\n");
+            System.out.println(UserInfo.get("Fox Davenport") + "\n"); 
     } 
     
     if(user==2) {
         System.out.println("Mithil's info: ");
-        System.out.println(UserInfo.get("Mithil") + "\n");
+        System.out.println(UserInfo.get("Mithil Pujar") + "\n");
     } 
 
         if(user==3) {
             System.out.println("Daniel's info: ");
-            System.out.println(UserInfo.get("Daniel Yang"));
-            System.out.println("Their password is: " + Password.get("danielyang@yahoo.com") + "\n");
+            System.out.println(UserInfo.get("Daniel Yang") + "\n");
+            
     }
     
     if(user==4) {
@@ -76,7 +94,7 @@ public class HashData
     
     if(user==5) {
         System.out.println("Alex's info: ");
-        System.out.println(UserInfo.get("Alex") + "\n");
+        System.out.println(UserInfo.get("Alex Clanton") + "\n");
     } 
 }
 }
